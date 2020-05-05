b815059ad09b427e56e39c33fa1c9b6855397686
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-05-06 02:08:05 +0800

    Update humban readable color

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate image;
+extern crate rand;
 
 use image::GenericImageView;
 use image::ImageBuffer;
+use rand::Rng;
 use std::cmp;
 
 fn main() {
@@ -26,6 +28,7 @@ fn main() {
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         *pixel = image::Rgb([255, 255, 255]);
     }
+    let mut curret_color = get_random_color();
     for i in 0..points.len() {
         for j in 0..points[i].len() {
             if curret_point[i][j] {
@@ -58,14 +61,14 @@ fn main() {
                     if neighbor_count >= min_ptx {
                         //if neighbor_count_check was hold
                         neighbor_count_check += 1;
-                        *imgbuf.get_pixel_mut(p[1] as u32, p[0] as u32) =
-                            image::Rgb(get_current_color(k));
+                        *imgbuf.get_pixel_mut(p[1] as u32, p[0] as u32) = image::Rgb(curret_color);
 
                         //P = color
                     }
                 }
                 if neighbor_count_check > 0 {
                     k += 1;
+                    curret_color = get_random_color();
                 }
                 curret_point[i][j] = false;
             }
@@ -78,7 +81,7 @@ fn get_eculid_distance(start_x: i32, start_y: i32, end_x: i32, end_y: i32) -> f6
     return (((start_x - end_x).pow(2) + (start_y - end_y).pow(2)) as f64).sqrt();
 }
 
-fn get_current_color(k: i32) -> [u8; 3] {
-    let color = (255 / (k + 1)) as u8;
-    [color, color, color]
+fn get_random_color() -> [u8; 3] {
+    let mut rng = rand::thread_rng();
+    [rng.gen(), rng.gen(), rng.gen()]
 }
